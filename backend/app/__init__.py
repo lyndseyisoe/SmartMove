0516570b8c59da137969d3cef4264214ae5f1e02
lyndsey isoe <lyndsey.nyanduko512@gmail.com>
2026-08-21 23:46:37 +0300

    feat: add user authentication

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 
 from .config import Config
-from .extensions import cors, db, migrate
+from .extensions import bcrypt, cors, db, jwt, migrate
 from .models.booking import Booking
 from .bookings.routes import bookings_bp
+from .users.model import User
+from .users.routes import auth_bp
 
 
 def create_app(config_class=Config):
@@ -14,8 +16,11 @@ def create_app(config_class=Config):
     db.init_app(app)
     migrate.init_app(app, db)
     cors.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
 
     app.register_blueprint(bookings_bp)
+    app.register_blueprint(auth_bp)
 
     @app.get("/")
     def health_check():
